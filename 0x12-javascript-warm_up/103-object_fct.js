bed575d2c09ca443aac5abdc1c237402cb4c5bb8
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2024-05-04 12:36:19 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
+//  new function incr that increments the integer value.
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-myObject.incr = () => { myObject.value += 1; };
+myObject.incr = function () {
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
