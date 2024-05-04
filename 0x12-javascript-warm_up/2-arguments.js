4373c68676f25591642c1d8f54d81a6054674c0d
Vhugala Mutshembele <100471016+VhugalaGalaxie@users.noreply.github.com>
2024-05-04 12:39:12 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('process');
-if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+// Prints a message depending of the number of arguments passed
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
